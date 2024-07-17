4549718fa86198f9a7e20c6ea8c21eb82af80fe2
bjartur2004 <bjartur2004@gmail.com>
2024-07-17 20:19:35 +0000

    smol

--- ARF/slave_network_manager.py
+++ ARF/slave_network_manager.py
@@ -6,8 +6,8 @@ from socket import AF_INET,SOCK_STREAM,socket
 BUFFER_SIZE = 1024
 
 sobj = socket(AF_INET,SOCK_STREAM)
-sobj.connect(('192.168.0.225',32032))
+sobj.connect(('192.168.0.101',32032))
 while True:
     message = sobj.recv(BUFFER_SIZE).decode()
     if(message == "checkstatus"):
-        sobj.send(3)
+        sobj.send("3".encode())
