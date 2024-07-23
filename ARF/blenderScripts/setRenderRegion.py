3c3adeb925e8b3314d1fded12d8150cd1ca19795
bjartur2004 <bjartur2004@gmail.com>
2024-07-23 17:52:34 +0000

    setborder

--- ARF/blenderScripts/setRenderRegion.py
+++ ARF/blenderScripts/setRenderRegion.py
@@ -1,16 +1,18 @@
 import bpy
 import sys
 
-#argv = sys.argv
-#argv = argv[argv.index("--") + 1:]  
+argv = sys.argv
+argv = argv[argv.index("--") + 1:]  # get all args after "--"
 
+print(argv)  
 
-#bpy.data.scenes['Scene'].render.border_min_x = float(argv[0])
-#bpy.data.scenes['Scene'].render.border_max_x = float(argv[1])
-#bpy.data.scenes['Scene'].render.border_min_y = float(argv[2])
-#bpy.data.scenes['Scene'].render.border_max_y = float(argv[3])
+sc = bpy.data.scenes['Scene']
+sc.render.use_border = True
 
-bpy.data.scenes['Scene'].render.border_min_x = 0
-bpy.data.scenes['Scene'].render.border_max_x = 0.5
-bpy.data.scenes['Scene'].render.border_min_y = 0
-bpy.data.scenes['Scene'].render.border_max_y = 0.5
+sc.render.border_min_x = 0.5
+sc.render.border_max_x = 0.8
+sc.render.border_min_y = 0.5
+sc.render.border_max_y = 0.8
+bpy.ops.wm.save_mainfile()
+
+print("border set uwu")
